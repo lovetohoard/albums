74315dd6ef45561d2396c118f5eed15e95b4ac9f
4levity <3319205+4levity@users.noreply.github.com>
2026-02-28 17:10:44 -0800

    feat: check-album-art extracts bad embedded images

--- src/albums/checks/picture/check_album_art.py
+++ src/albums/checks/picture/check_album_art.py
@@ -1,9 +1,15 @@
 import logging
-from typing import Any
+import mimetypes
+from collections import defaultdict
+from typing import Any, Mapping
 
 import humanize
 
-from ...types import Album, CheckResult
+from albums.checks.helpers import FRONT_COVER_FILENAME
+from albums.interactive.image_table import render_image_table
+
+from ...tagger.types import Picture, PictureType
+from ...types import Album, CheckResult, Fixer
 from ..base_check import Check
 
 logger = logging.getLogger(__name__)
@@ -13,7 +19,8 @@ class CheckAlbumArt(Check):
     name = "album-art"
     default_config = {
         "enabled": True,
-        "embedded_size_max": 8 * 1024 * 1024,  # up to 16 MB is OK in ID3v2
+        "embedded_size_max": (4 * 1024 * 1024) - 1,  # up to 16 MB is OK in ID3v2
+        # TODO rules for non-embedded album art
     }
     must_pass_checks = {"invalid-image"}
 
@@ -21,22 +28,64 @@ class CheckAlbumArt(Check):
         self.embedded_size_max = int(check_config.get("embedded_size_max", CheckAlbumArt.default_config["embedded_size_max"]))
 
     def check(self, album: Album) -> CheckResult | None:
-        issues: set[str] = set()
-        album_art = [(track.filename, True, track.pictures) for track in album.tracks]
-        album_art.extend([(filename, False, [file.picture]) for filename, file in album.picture_files.items()])
+        picture_sources: defaultdict[Picture, list[tuple[str, bool, int]]] = defaultdict(list)
+        bad_formats: list[str] = []
+        bad_file_sizes = 0
+        largest_bad_file_size = 0
+        for track in album.tracks:
+            for embed_ix, picture in enumerate(track.pictures):
+                if picture.file_info.mime_type not in {"image/png", "image/jpeg"}:
+                    picture_sources[picture].append((track.filename, True, embed_ix))
+                    bad_formats.append(picture.file_info.mime_type)
+                if picture.file_info.file_size > self.embedded_size_max:
+                    picture_sources[picture].append((track.filename, True, embed_ix))
+                    largest_bad_file_size = max(largest_bad_file_size, picture.file_info.file_size)
+                    bad_file_sizes += 1
 
-        for _filename, embedded, pictures in album_art:
-            for picture in pictures:
-                if embedded:
-                    if picture.file_info.mime_type not in {"image/png", "image/jpeg"}:
-                        # TODO: extract original to file, then automatically convert to jpg
-                        issues.add(f"embedded image {picture.type.name} is not a recommended format ({picture.file_info.mime_type})")
-                    if picture.file_info.file_size > self.embedded_size_max:
-                        # TODO: extract original to file, then resize/compress
-                        file_size = humanize.naturalsize(picture.file_info.file_size, binary=True)
-                        file_size_max = humanize.naturalsize(self.embedded_size_max, binary=True)
-                        issues.add(f"embedded image {picture.type.name} is over the configured limit ({file_size} > {file_size_max})")
-            # TODO apply other configurable rules to all album art
+        if picture_sources:
+            issues: list[str] = []
+            if bad_formats:
+                issues.append(f"embedded images ({len(bad_formats)}) not in recommended format ({', '.join(set(bad_formats))})")
+            if bad_file_sizes:
+                file_size = humanize.naturalsize(largest_bad_file_size, binary=True)
+                file_size_max = humanize.naturalsize(self.embedded_size_max, binary=True)
+                issues.append(f"embedded images ({bad_file_sizes}) over the size limit (largest {file_size} > {file_size_max})")
+            options = [">> Extract images to files and remove embedded"]
+            option_automatic_index = 0
+            extract = list(picture_sources.items())
+            table = (
+                [f"{pic.type.name} in {len(refs)} files" for pic, refs in extract],
+                lambda: render_image_table(self.ctx, self.tagger.get(album.path), [pic for pic, _ in extract], picture_sources),
+            )
+            return CheckResult(
+                ", ".join(list(issues)),
+                Fixer(
+                    lambda _: self._fix_extract(album, picture_sources),
+                    options,
+                    False,
+                    option_automatic_index,
+                    table,
+                ),
+            )
 
-        if issues:
-            return CheckResult(", ".join(list(issues)))
+    def _fix_extract(self, album: Album, embedded_to_extract: Mapping[Picture, list[tuple[str, bool, int]]]):
+        tagger = self.tagger.get(album.path)
+        for pic, refs in embedded_to_extract.items():
+            (filename, _, embed_ix) = refs[0]
+            stem = FRONT_COVER_FILENAME if pic.type == PictureType.COVER_FRONT else str.lower(pic.type.name)
+            suffix = mimetypes.guess_extension(pic.file_info.mime_type)
+            num = 0
+            while (new_file := (self.ctx.config.library / album.path / f"{stem}{f'{num}' if num else ''}{suffix}")) and new_file.exists():
+                num += 1
+
+            self.ctx.console.print(f"Extracting {pic.type.name} {pic.file_info.mime_type} from {filename} to {new_file.name}")
+            with tagger.open(filename) as tags:
+                image_data = tags.get_image_data(pic.type, embed_ix)
+            with open(new_file, "wb") as f:
+                f.write(image_data)
+            for filename, _, embed_ix in refs:
+                with tagger.open(filename) as tags:
+                    self.ctx.console.print(f"Removing {pic.type.name} {pic.file_info.mime_type} from {filename}")
+                    tags.remove_picture(pic)
+
+        return True
--- tests/checks/picture/test_check_album_art.py
+++ tests/checks/picture/test_check_album_art.py
@@ -1,8 +1,14 @@
+from pathlib import Path
+from unittest.mock import call, mock_open, patch
+
 from albums.app import Context
 from albums.checks.picture.check_album_art import CheckAlbumArt
 from albums.picture.info import PictureInfo
-from albums.tagger.types import Picture, PictureType, StreamInfo
+from albums.tagger.folder import AlbumTagger
+from albums.tagger.types import Picture, PictureType, StreamInfo, TaggerFile
 from albums.types import Album, Track
+
+from ...fixtures.create_library import make_image_data
 
 
 class TestCheckAlbumArt:
@@ -23,7 +29,7 @@ class TestCheckAlbumArt:
         result = CheckAlbumArt(Context()).check(album)
         assert result is None
 
-    def test_album_art_format(self):
+    def test_album_art_format(self, mocker):
         album = Album(
             "",
             [
@@ -33,13 +39,33 @@ class TestCheckAlbumArt:
                     0,
                     0,
                     StreamInfo(1.5, 0, 0, "FLAC"),
-                    [Picture(PictureInfo("image/gif", 400, 400, 24, 1024, b""), PictureType.COVER_FRONT, "", ())],
+                    [Picture(PictureInfo("image/gif", 400, 400, 8, 1024, b""), PictureType.COVER_FRONT, "", ())],
                 )
             ],
         )
         result = CheckAlbumArt(Context()).check(album)
         assert result is not None
-        assert result.message == "embedded image COVER_FRONT is not a recommended format (image/gif)"
+        assert result.message == "embedded images (1) not in recommended format (image/gif)"
+        assert result.fixer
+        assert result.fixer.options == [">> Extract images to files and remove embedded"]
+        assert result.fixer.option_automatic_index == 0
+
+        tagger = TaggerFile()
+        image_data = make_image_data(400, 400, "GIF")
+        mock_tagger_open = mocker.patch.object(AlbumTagger, "open")
+        mock_tagger_open.return_value.__enter__.return_value = tagger
+        mock_get_image_data = mocker.patch.object(tagger, "get_image_data", return_value=image_data)
+
+        m_open = mock_open()
+        with patch("builtins.open", m_open):
+            assert result.fixer.get_table()
+            assert mock_get_image_data.call_count == 1
+            result.fixer.fix(result.fixer.options[result.fixer.option_automatic_index])
+        assert mock_get_image_data.call_count == 2
+        mock_handle = m_open.return_value
+        image_data_written = mock_handle.write.call_args[0][0]
+        m_open.assert_has_calls([call(Path(".") / album.path / "cover.gif", "wb")])
+        assert image_data_written == image_data
 
     def test_album_art_file_too_large(self):
         album = Album(
@@ -57,4 +83,4 @@ class TestCheckAlbumArt:
         )
         result = CheckAlbumArt(Context()).check(album)
         assert result is not None
-        assert result.message == "embedded image COVER_FRONT is over the configured limit (15.0 MiB > 8.0 MiB)"
+        assert result.message == "embedded images (1) over the size limit (largest 15.0 MiB > 4.0 MiB)"
