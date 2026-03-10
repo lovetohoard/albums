92d12ab038e8238d25e8f053797b51fcb710890b
4levity <3319205+4levity@users.noreply.github.com>
2026-03-10 13:32:13 -0700

    fix: 'albums sql' fix error when no rows

--- src/albums/cli/sql.py
+++ src/albums/cli/sql.py
@@ -19,22 +19,27 @@ def sql(ctx: Context, sql_command: str, json: bool):
     try:
         with ctx.db.begin() as connection:
             cursor = connection.execute(text(sql_command))
-            if json:
-                json_dump = dumps([[v for v in row] for row in cursor.fetchall()])
-                ctx.console.print_json(json_dump)
-                # ctx.console.print_json(dumps([row[0] for row in cursor.fetchall()]))
-                # more compact: ctx.console.print(dumps(cursor.fetchall()))
+            if cursor.returns_rows:
+                if json:
+                    json_dump = dumps([[v for v in row] for row in cursor.fetchall()])
+                    ctx.console.print_json(json_dump)
+                    # more compact: ctx.console.print(dumps(json_dump))
+                else:
+                    column_names = list(
+                        [
+                            str(description[0])
+                            for description in (cursor.cursor.description if cursor.cursor and cursor.cursor.description else [("results",)])
+                        ]
+                    )
+                    table = Table(*column_names)
+                    for row in cursor:
+                        table.add_row(*[escape(str(v) + " ").strip() for v in row])
+                    ctx.console.print(table)
+            elif json:
+                ctx.console.print("[]")
             else:
-                column_names = list(
-                    [
-                        str(description[0])
-                        for description in (cursor.cursor.description if cursor.cursor and cursor.cursor.description else [("results",)])
-                    ]
-                )
-                table = Table(*column_names)
-                for row in cursor:
-                    table.add_row(*[escape(str(v) + " ").strip() for v in row])
-                ctx.console.print(table)
+                ctx.console.print("(no rows returned)")
+
             connection.commit()
     except OperationalError as err:
         ctx.console.print(Panel(f"[bold]SQL error | [red]{escape(str(err.orig))}", expand=False))
